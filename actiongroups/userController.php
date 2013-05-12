f319c147fa43f9a28745abdbd833a752193cc86b
E-Wok <n.beauvais78@gmail.com>
2013-05-12 07:37:59 +0200

    update controler

--- actiongroups/userController.php
+++ actiongroups/userController.php
@@ -4,28 +4,33 @@ include('./models/userModel.php');
 
 if($action == 'user'){
 
-    $user = intval($_GET['user'], 10);
+    if(!empty($_GET['user'])){
+        $user = intval($_GET['user'], 10);
 
-    if($data = getUserData($user)){
+        if($data = getUserData($user)){
 
-        $total = 0;
+            $total = 0;
 
-        foreach($data as $key=>$elem){
-            $array = array_reverse(json_decode($data[$key]['lan'], true));
-            arsort($array, SORT_NUMERIC);
-            $data[$key]['lan'] = $array;
+            foreach($data as $key=>$elem){
+                $array = array_reverse(json_decode($data[$key]['lan'], true));
+                arsort($array, SORT_NUMERIC);
+                $data[$key]['lan'] = $array;
+            }
+
+            foreach($data[$key]['lan'] as $value){
+                $total += $value;
+            }
+
+            $data[0]['total'] = $total;
+
+
+            $smarty->assign('data', $data[0]);
+
+            $tpl = 'user';
         }
-
-        foreach($data[$key]['lan'] as $value){
-            $total += $value;
+        else{
+            $tpl = 'home';
         }
-
-        $data[0]['total'] = $total;
-
-
-        $smarty->assign('data', $data[0]);
-
-        $tpl = 'user';
     }
     else{
         $tpl = 'home';
