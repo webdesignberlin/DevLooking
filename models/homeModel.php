6fe29742bb830c59a7e01e706e71b6f79d13e0a2
E-Wok <n.beauvais78@gmail.com>
2013-05-12 02:49:37 +0200

    update homeModel

--- models/homeModel.php
+++ models/homeModel.php
@@ -29,6 +29,7 @@
         $blog = mysqli_real_escape_string($link, $user['blog']);
         $location = mysqli_real_escape_string($link, $user['location']);
         $email = mysqli_real_escape_string($link, $user['email']);
+        $hireable = mysqli_real_escape_string($link, $user['hireable']);
         $bio = mysqli_real_escape_string($link, $user['bio']);
         $public_repos = mysqli_real_escape_string($link, $user['public_repos']);
         $followers = mysqli_real_escape_string($link, $user['followers']);
@@ -37,8 +38,7 @@
 
         $score = getScore($company, $public_repos, $followers, $following, $public_gists);
 
-        $query = 'INSERT INTO `users` VALUES ("'.$id.'","'.$login.'","'.$avatar_url.'","'.$html_url.'","'.$name.'","'.$company.'","'.$blog.'","'.$location.'","'.$email.'","'.$bio.'","'.$score.'")';
-
+        $query = 'INSERT INTO `users` VALUES ("'.$id.'","'.$login.'","'.$avatar_url.'","'.$html_url.'","'.$name.'","'.$hireable.'","'.$company.'","'.$blog.'","'.$location.'","'.$email.'","'.$bio.'","'.$score[2].'","'.$score[0].'","'.$score[1].'","'.$score[3].'")';
 
         myQueryExec($query);
 
@@ -58,6 +58,7 @@
         $blog = mysqli_real_escape_string($link, $user['blog']);
         $location = mysqli_real_escape_string($link, $user['location']);
         $email = mysqli_real_escape_string($link, $user['email']);
+        $hireable = mysqli_real_escape_string($link, $user['hireable']);
         $bio = mysqli_real_escape_string($link, $user['bio']);
         $public_repos = mysqli_real_escape_string($link, $user['public_repos']);
         $followers = mysqli_real_escape_string($link, $user['followers']);
@@ -66,35 +67,85 @@
 
         $score = getScore($company, $public_repos, $followers, $following, $public_gists);
 
+        $score[3] = mysqli_real_escape_string($link, $score[3]);
+
         $query = 'UPDATE `users`
-        SET id="'.$id.'", login="'.$login.'", avatar_url="'.$avatar_url.'", html_url="'.$html_url.'", name="'.$name.'", company="'.$company.'", blog="'.$blog.'", location="'.$location.'", email="'.$email.'", bio="'.$bio.'", score="'.$score.'" WHERE id = "'.$id.'"';
+        SET id="'.$id.'", login="'.$login.'", avatar_url="'.$avatar_url.'", html_url="'.$html_url.'", name="'.$name.'", company="'.$company.'", blog="'.$blog.'", location="'.$location.'", email="'.$email.'", hireable="'.$hireable.'", bio="'.$bio.'", score="'.$score[2].'", personal_score="'.$score[0].'", repos_score="'.$score[1].'", lan="'.$score[3].'" WHERE id = "'.$id.'"';
 
         myQueryExec($query);
 
         return $score;
     }
 
+    function getRepos(){
+        global $client;
+
+        $url = 'https://api.github.com/user/repos';
+        $method = 'GET';
+        $parameters = array();
+        $options = array();
+
+        $success = $client->CallAPI($url, $method, $parameters, $options, $repos);
+
+        $rs = 0;
+        $lan = array();
+        $resultLan = array();
+
+        foreach($repos as $key=>$repo){
+            $ar = get_object_vars($repo);
+
+            if(!$ar['fork']){
+                $rs += ($ar['size'] * 2);
+            }
+
+            $rs += ($ar['watchers'] * 350);
+            $rs += ($ar['open_issues_count'] * -20);
+
+            if($ar['has_downloads']){
+                $rs += 400;
+            }
+
+            if($ar['has_wiki']){
+                $rs += 350;
+            }
+
+            $rs += ($ar['forks_count'] * 345);
+
+
+            if(!in_array($ar['language'], $lan)){
+                $resultLan[$ar['language']] = 1;
+            }
+            else{
+                $resultLan[$ar['language']] += 1;
+            }
+
+            $lan[$key] = $ar['language'];
+        }
+
+        $languages = json_encode($resultLan);
+
+        $result = array($rs, $languages);
+        return $result;
+    }
+
     function getScore($company, $public_repos, $followers, $following, $public_gists){
+
+        $reposScore = getRepos();
 
         $score = 0;
 
         if(!empty($company)){
-            $score .= 95;
+            $score += 95;
         }
-        echo $score.'<br>';
-        $score .= (intval($public_repos, 10) * 470);
-        echo $score.'<br>';
-        $score .= (intval($followers, 10) * 186);
-        echo $score.'<br>';
 
-        $score .= (intval($following, 10) * 125);
-        echo $score.'<br>';
+        $score += (intval($public_repos, 10) * 270);
+        $score += (intval($followers, 10) * 186);
+        $score += (intval($following, 10) * 125);
+        $score += (intval($public_gists, 10) * 185);
 
-        $score .= (intval($public_gists, 10) * 185);
-        echo $score.'<br>';
+        $return = array($score, $reposScore[0], $reposScore[0] + $score, $reposScore[1]);
 
-
-        return $score;
+        return $return;
 
     }
 
@@ -108,5 +159,6 @@
         $_SESSION['location'] = $user['location'];
         $_SESSION['email'] = $user['email'];
         $_SESSION['score'] = $user['score'];
+        $_SESSION['hireable'] = $user['hireable'];
     }
 ?>
